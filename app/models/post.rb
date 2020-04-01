28ef433abe61c1552f31c3aa7d14b922bac7899a
crstn <cristiancarrasco80@gmail.com>
2020-03-31 21:48:46 -0300

    arreglando errores

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,5 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :image_url, presence: true
   validates :content, presence: true
-  scope :order_by_date, -> {order(created_at: :desc)}
+  scope :order_by_date, -> {order(created_at: :desc)} 
 end
